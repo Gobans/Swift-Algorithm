e2c30a9e1f0107976a6de51d046a1cecc3f7ff48
Goban <gobanest@gmail.com>
2023-01-17 00:08:38 +0900

    :memo: 프로그래머스 전력망을 둘로 나누기 BFS
    :pencil2:

--- SwiftAlgorithm/Programmers/고득점Kit/전력망을 둘로 나누기/main.swift
+++ SwiftAlgorithm/Programmers/고득점Kit/전력망을 둘로 나누기/main.swift
@@ -11,6 +11,7 @@ func solution(_ n:Int, _ wires:[[Int]]) -> Int {
     // DFS, 전선을 하나 끊었을 떄 하나의 간선이 몇개 이어져있는지 확인
     // 전체 송전탑 - 이어진 송전탑 = 또 다른 송전탑의 갯수
     // 두개의 송전탑 개수의 차이
+    var minDiff = 100
     func BFS(towerGraph: [[Bool]], k: Int) {
         var visited = Array(repeating: false, count: n + 1)
         var stack: [Int] = []
@@ -18,9 +19,6 @@ func solution(_ n:Int, _ wires:[[Int]]) -> Int {
         visited[1] = true
         while !stack.isEmpty {
             let checkPoint = stack.removeLast()
-            if k == 5 {
-                print("checkPoint \(checkPoint)")
-            }
             for i in 1...n {
                 if !visited[i] && towerGraph[checkPoint][i] {
                     visited[i] = true
@@ -30,14 +28,10 @@ func solution(_ n:Int, _ wires:[[Int]]) -> Int {
         }
         let firstTowers = visited.filter{ $0 == true }.count
         let secondTowers = n - firstTowers
-        if k == 5 {
-            print(towerGraph)
+        let diff = abs(firstTowers - secondTowers)
+        if diff < minDiff {
+            minDiff = diff
         }
-        print("time: \(k)")
-        print("visited \(visited)")
-        print("firstTowers \(firstTowers)")
-        print("secondTowers \(secondTowers)")
-        print(abs(firstTowers - secondTowers))
     }
     for i in 0..<wires.count {
         var cWires = wires
@@ -48,13 +42,10 @@ func solution(_ n:Int, _ wires:[[Int]]) -> Int {
             let startPoint = wire[0]
             let endPoint = wire[1]
             towerGraph[startPoint][endPoint] = true
+            towerGraph[endPoint][startPoint] = true
         }
-        BFS(towerGraph: towerGraph, k: i)
+        BFS(towerGraph: towerGraph, k: i + 1)
     }
     
-    return -1
+    return minDiff
 }
-
-let n = 9
-let wires = [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]
-solution(n, wires)
