75a90927be74cc7c8055546df1f6e6b684d5dd14
Goban <gobanest@gmail.com>
2023-02-22 22:03:08 +0900

    :memo: 프로그래머스 고득점 Kit 방의개수
    update README.md

---
+++ SwiftAlgorithm/Programmers/고득점Kit/방의 개수/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by Lee Myeonghwan on 2023/02/22.
+//
+
+import Foundation
+
+func solution(_ arrows:[Int]) -> Int {
+    // 대각선의 움직임을 나타내기 위해 움직임을 2배로 만들기?
+    // 여러방이 있는지 어떻게 확인하는가?
+    // 방이 만들어지는 조건: 지나왔던 좌표를 또 방문했을때
+    // -> 1. 넓게 어떠한 도형으로 만들어 져서 도착했을때
+    // -> 2. 대각선으로 와서 세모의 형태로 만들어 졌을때
+    return 0
+}
