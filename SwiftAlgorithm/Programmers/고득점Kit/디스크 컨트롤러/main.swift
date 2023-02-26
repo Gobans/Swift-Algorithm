8bab4a4901e9d38d6857ddbc898882f0f74a0675
Goban <gobanest@gmail.com>
2023-02-26 22:37:09 +0900

    :memo: 프로그래머스 디스크 컨트롤러 refactoring
    update README.md

--- SwiftAlgorithm/Programmers/고득점Kit/디스크 컨트롤러/main.swift
+++ SwiftAlgorithm/Programmers/고득점Kit/디스크 컨트롤러/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/* `sort`
+// `sort`
 func solution(_ jobs:[[Int]]) -> Int {
     var now = 0
     var totalTime = 0
@@ -30,7 +30,6 @@ func solution(_ jobs:[[Int]]) -> Int {
     }
     return totalTime / jobs.count
 }
-*/
 
 /* `heap`
 func solution(_ jobs:[[Int]]) -> Int {
@@ -61,3 +60,27 @@ func solution(_ jobs:[[Int]]) -> Int {
     return totalTime / jobs.count
 }
  */
+
+/* `heap refactor: more readable`
+func solution(_ jobs:[[Int]]) -> Int {
+    var now = 0
+    var totalTime = 0
+    var cJobs = jobs.sorted(by: { $0[0] > $1[0] })
+    var sortedJobs = Heap (sort: { (lhs: [Int], rhs: [Int]) in
+        return (lhs[1], lhs[0]) < (rhs[1], rhs[0])
+    })
+    while !cJobs.isEmpty || !sortedJobs.isEmpty {
+        while !cJobs.isEmpty && cJobs.last![0] <= now {
+            sortedJobs.insert(cJobs.removeLast())
+        }
+        if !sortedJobs.isEmpty {
+            let job = sortedJobs.remove()!
+            now += job[1]
+            totalTime += now - job[0]
+        } else {
+            now += 1
+        }
+    }
+    return totalTime / jobs.count
+}
+*/
