c2ffb2094f707e0df51e094fbfdb002c19c9d7f6
Goban <gobanest@gmail.com>
2023-02-27 17:22:44 +0900

    :pencil2: 프로그래머스 섬 연결하기 코드 새롭게 작성
    :pencil2:

--- SwiftAlgorithm/Programmers/고득점Kit/섬연결하기/main.swift
+++ SwiftAlgorithm/Programmers/고득점Kit/섬연결하기/main.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// Solution referenced by other people's code
 var parentNumber: [Int] = []
 
 func solution(_ n:Int, _ costs:[[Int]]) -> Int {
@@ -31,3 +32,24 @@ func isCycle(left:Int, right:Int) -> Bool {
 func changeParent(oldParent:Int,newParent:Int) {
     parentNumber.indices.filter {parentNumber[$0] == oldParent}.forEach{parentNumber[$0] = newParent}
 }
+
+/* Newly writed solution
+import Foundation
+
+func solution(_ n:Int, _ costs:[[Int]]) -> Int {
+    var sum = 0
+    var parentNumber: [Int] = (0..<n).map{ $0 }
+    let costs = costs.sorted { lhs, rhs in
+        return lhs[2] < rhs[2]
+    }
+    for i in 0..<costs.count {
+        let (islandOne, islandTwo) = (costs[i][0], costs[i][1])
+        if parentNumber[islandOne] != parentNumber[islandTwo] {
+            sum += costs[i][2]
+            parentNumber.indices.filter{ parentNumber[$0] == parentNumber[islandTwo] }.forEach{ parentNumber[$0] = parentNumber[islandOne]
+            }
+        }
+    }
+    return sum
+}
+ */
